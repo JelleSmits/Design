a1ae438c332de8e4da35ed239a7a1097368025b5
JelleSmits <j.j.smits@student.utwente.nl>
2015-12-02 08:40:17 +0100

    Design Week 4

---
+++ DesignLectureTest/src/week3/hotel/Password.java
@@ -0,0 +1,39 @@
+package week3.hotel;
+
+import week3.BasicChecker;
+import week3.Checker;
+import week3.StrongChecker;
+
+public class Password {
+	
+	public static final String INITIAL = "admins";
+	private String password;
+	
+	public Password() {
+		password = INITIAL;
+	}
+	
+	public boolean acceptable(String suggestion) {
+		return ((suggestion.length() >= 6) && !(suggestion.contains(" ")));
+	}
+	
+	public boolean testWord(String test) {
+		return (test.equals(password));
+	}
+	
+	public boolean setWord(String oldpass, String newpass) {
+		if (acceptable(newpass) && oldpass.equals(password)) {
+			password = newpass;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public String getPass() {
+		return password;
+	}
+	
+	
+}
---
+++ DesignLectureTest/src/week3/hotel/PricedRoom.java
@@ -0,0 +1,23 @@
+package week3.hotel;
+
+import week3.Format;
+
+public class PricedRoom extends Room implements Bill.Item {
+	
+	private double price;
+	
+	public PricedRoom(int number, double roomPrice, double safePrice) {
+		super(number, new PricedSafe(safePrice));
+		price = roomPrice;
+	}
+	
+	public double getAmount() {
+		return price;
+	}
+	
+	public String toString() {
+		return Format.printLine("Price of the room per night: €", price);
+	}
+	
+
+}
